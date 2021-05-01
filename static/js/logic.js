d603814c1fc33fcfd072a01c5a0de60ff2cec0fb
CarmiSmith <75535889+CarmiSmith@users.noreply.github.com>
2021-05-01 16:05:18 -0400

    final draft

--- static/js/logic.js
+++ static/js/logic.js
@@ -133,7 +133,7 @@ function createMap(earthquakes) {
       var div = L.DomUtil.create('div', 'info legend');
       grades = [-10, 10, 30, 50, 70];
       //labels = [];
-      colors = ["#008000", "#ffff00", "#0000ff", "orange", "red"]; 
+      colors = ["green", "yellow", "blue", "orange", "red"]; 
           
         for (var i = 0; i < colors.length; i++) {
             div.innerHTML +=
